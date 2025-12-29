7a645126de29753986ba28d5388c6855a09cd725
Appropriate32 <frostghost32@gmail.com>
2025-12-29 21:35:48 +0500

    Added remove todo functionality

--- src/index.js
+++ src/index.js
@@ -24,13 +24,26 @@ class ProjectManager {
         return newProject; // Return data object to DOM handler
     }
 
-    addTodoToProject(projectId, title, due, priority, description) {
-        const newTodo = { title, due, priority, description};
+    addTodoToProject(projectId, todoId, title, due, priority, description) {
+        const newTodo = {todoId, title, due, priority, description};
         const project = this.projects.find(p => p.id === projectId);
         if (project) {
             project.todos.push(newTodo);
         }
         return newTodo;
+    }
+
+    removeTodoFromProject(projectId, todoId) {
+        const project = this.projects.find(p => p.id === projectId);
+        
+        if (project) {
+            const todoIndex = project.todos.findIndex(todo => todo.todoId === todoId);
+            
+            if (todoIndex !== -1) {
+                project.todos.splice(todoIndex, 1);
+            }
+            
+        }
     }
 }
 
@@ -44,14 +57,21 @@ class DomStuff {
         this.projectSection = document.querySelector(".project-section");
         this.addProjectBtn = document.querySelector(".add-project");
         this.buttonsContainer = document.querySelector(".buttons");
+        this.removeTodo = document.querySelector(".remove-todo");
 
         this.init();
     }
 
     init() {
-        // Todo Listener
+        // Todo Listeners
         this.todoButton.addEventListener("click", () => this.renderNewToDo());
         
+        this.mainContent.addEventListener("click", (e) => {
+            if (e.target.classList.contains("remove-todo")) {
+                this.handleRemoveTodo(e);
+            }
+        });
+
         // Project Listeners
         if (this.addProjectBtn) {
             this.addProjectBtn.addEventListener("click", () => this.handleAddProject());
@@ -62,8 +82,8 @@ class DomStuff {
         }
     }
 
-    renderNewToDo(projectId = 1) {
-        const data = this.ProjectManager.addTodoToProject(projectId, "New Task", "Jan 10", "Low", "Desc");
+    renderNewToDo(projectId = 1, todoId) {
+        const data = this.ProjectManager.addTodoToProject(projectId, todoId ,"New Task", "Jan 10", "Low", "Desc");
         this.createTodoElement(data);
     }
 
@@ -99,13 +119,25 @@ class DomStuff {
 
         this.refreshMainContent(projectId);
     }
+
+    handleRemoveTodo(e) {
+        const todoItem = e.target.closest(".todo-item");
+        const todoId = todoItem.getAttribute("data-todo-id");
+
+        const activeProject = document.querySelector(".project-container.active");
+        const projectId = Number(activeProject.dataset.id);
+
+        this.ProjectManager.removeTodoFromProject(projectId, todoId);
+
+        todoItem.remove();
+    }
     
     refreshMainContent(projectId) {
         this.mainContent.innerHTML = '<button class="add-todo">Add Todo</button>';
 
         this.todoButton = document.querySelector(".add-todo");
 
-        this.todoButton.addEventListener("click", () => this.renderNewToDo(projectId));
+        this.todoButton.addEventListener("click", () => this.renderNewToDo(projectId, crypto.randomUUID()));
 
         const project = this.ProjectManager.projects.find(p => p.id === projectId);
 
@@ -119,6 +151,9 @@ class DomStuff {
     createTodoElement(data) {
         const todoItem = document.createElement("div");
         todoItem.classList.add("todo-item");
+        
+        todoItem.setAttribute("data-todo-id", data.todoId);
+        
         todoItem.innerHTML = `
             <h2 class="todo-heading">${data.title}</h2>
             <p class="due">Due: ${data.due}</p>
